672bf91516975d157e95ac478d054bb556893f9e
paper-piper <113584610+paper-piper@users.noreply.github.com>
2024-10-08 08:29:40 +0300

    improved thread testing

--- tests/threading_test.py
+++ tests/threading_test.py
@@ -1,6 +1,8 @@
 import threading
-
+from logging_utils import setup_logger
 from database.synchronizer_db import SynchronizerDB, SyncState
+
+logger = setup_logger('threading_test')
 
 
 FILENAME = "assertions_test.pickle"
@@ -8,28 +10,35 @@ MAX_READERS = 10
 THREADS_NUM = 10
 
 
-def simple_db_test(sync_db):
+def simple_db_test(sync_db, index):
 
-    assert not sync_db.get_value(1)
+    results = sync_db.get_value(index)
+    logger.info(f"on thread {index} first value = {results}")
+    assert not results
 
-    sync_db.set_value(1, True)
-    assert sync_db.get_value(1)
+    sync_db.set_value(index, True)
+    results = sync_db.get_value(index)
+    logger.info(f"on thread {index} seconds value = {results}")
+    assert results
 
-    assert sync_db.delete_value(1)
+    #assert sync_db.delete_value(index)
 
-    assert sync_db.get_value(1) is None
+    #assert sync_db.get_value(index) is None
 
 
 def assert_synchronizer_threads():
+    db = {}
+    for i in range(10):
+        db[i] = False
     sync_db = SynchronizerDB(
         FILENAME,
         SyncState.THREADS,
         MAX_READERS,
-        {1: False}
+        db
     )
     threads = []
-    for i in range(THREADS_NUM):
-        threads.append(threading.Thread(target=simple_db_test,args=(sync_db,)))
+    for i in range(1, THREADS_NUM):
+        threads.append(threading.Thread(target=simple_db_test, args=(sync_db, i)))
 
     for thread in threads:
         thread.start()
