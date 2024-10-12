1541d6226ca14c1fec8a4b94dda256e715006d8a
yoni kisho <yonatanreichert@gmail.com>
2024-10-12 17:43:52 +0300

    made working final multiprocessing test

--- tests/processing_test.py
+++ tests/processing_test.py
@@ -6,53 +6,58 @@ logger = setup_logger('multiprocessing_test')
 
 FILENAME = "assertions_test.pickle"
 MAX_READERS = 10
-PROCESSES_NUM = 10
+READERS_NUM = 10
+WRITERS_NUM = 20
+LOOP_TIMES = 10
+DATABASE_LENGTH = 20
 
 
-def simple_db_test(sync_db, index):
+def reader_work(sync_db, key):
+    for i in range(LOOP_TIMES):
+        sync_db.get_value(key)
+
+
+def writer_work(sync_db, key, value):
+    for i in range(LOOP_TIMES):
+        sync_db.set_value(key, value)
+
+
+def special_writer_work(sync_db):
+    for i in range(LOOP_TIMES):
+        sync_db.set_value(i % (DATABASE_LENGTH // 2), 1)
+
+
+def assert_synchronizer_multiprocessing():
     """
-    a simple set of instructions for the database
-    :param sync_db:
-    :param index:
+    Run the same simple db assertion using multiple processes.
     :return: None
     """
-
-    results = sync_db.get_value(index)
-    logger.info(f"on process {index} first value = {results}")
-    assert not results
-
-    sync_db.set_value(index, True)
-    results = sync_db.get_value(index)
-    logger.info(f"on process {index} second value = {results}")
-    assert results
-
-    assert sync_db.delete_value(index)
-
-    assert sync_db.get_value(index) is None
-
-
-def assert_synchronizer_processes():
-    """
-    make multiple processes run the same simple db assertion at the same time.
-    :return: None
-    """
-    # Multiprocessing manager to share the initial db dictionary across processes
-    manager = multiprocessing.Manager()
-    db = manager.dict()
-    for i in range(PROCESSES_NUM):
+    db = {}
+    for i in range(DATABASE_LENGTH):
         db[i] = False
-
     sync_db = SynchronizerDB(
         FILENAME,
-        SyncState.PROCESSES,
+        SyncState.PROCESSES,  # Assuming this supports processes
         MAX_READERS,
         db
     )
 
     processes = []
-    for i in range(PROCESSES_NUM):
-        p = multiprocessing.Process(target=simple_db_test, args=(sync_db, i))
+
+    for i in range(READERS_NUM):
+        # make processes work in range between 0-20
+        index = i % DATABASE_LENGTH
+        p = multiprocessing.Process(target=reader_work, args=(sync_db, index))
         processes.append(p)
+
+    for i in range(2, WRITERS_NUM):
+        # make processes work in range between 10-20
+        index = (i % (DATABASE_LENGTH // 2)) + DATABASE_LENGTH // 2
+        p = multiprocessing.Process(target=writer_work, args=(sync_db, index, i))
+        processes.append(p)
+
+    special_process = multiprocessing.Process(target=special_writer_work, args=(sync_db,))
+    processes.append(special_process)
 
     for process in processes:
         process.start()
@@ -60,6 +65,9 @@ def assert_synchronizer_processes():
     for process in processes:
         process.join()
 
+    for i in range(DATABASE_LENGTH):
+        print(sync_db.get_value(i))
+
 
 if __name__ == "__main__":
-    assert_synchronizer_processes()
+    assert_synchronizer_multiprocessing()
